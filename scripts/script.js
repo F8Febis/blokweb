b94c24bf35f211fbb61cffbf7ac89935e83e382e
F8Febis <70529530+F8Febis@users.noreply.github.com>
2020-09-29 01:08:15 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,24 @@ var Hamburger = document.querySelector("#hamburger");
 Hamburger.addEventListener("click", doeHeen);
 
 function doeHeen() {
-	let menu = document.querySelector("#menu");
-	menu.classList.add("toonHamburger");
-  
+    let menu = document.querySelector("#menu");
+    menu.classList.add("toonHamburger");
+
 }
 var kruisje = document.querySelector("header div > img");
 
 kruisje.addEventListener("click", doeWeer);
+
 function doeWeer() {
-	let menu = document.querySelector("#menu");
-	menu.classList.remove("toonHamburger");
-  
+    let menu = document.querySelector("#menu");
+    menu.classList.remove("toonHamburger");
+
 }
 
+var hartje = document.querySelector("nav ul:first-of-type li:nth-child(5) img");
+
+hartje.addEventListener("click", hartjekloppen);
+
+function hartjekloppen() {
+    hartje.classList.toggle("hart");
+}
